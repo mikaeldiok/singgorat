aa1bbdef2257cfa0fe723921a29841aa52a91ac2
mikaeldiok <mikaeldiok@gmail.com>
2023-11-13 09:57:40 +0700

    form be

--- Modules/Reporting/Resources/views/backend/reports/form.blade.php
+++ Modules/Reporting/Resources/views/backend/reports/form.blade.php
@@ -4,65 +4,6 @@
             <?php
             $field_name = 'reporter';
             $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
-            $required = "";
-            ?>
-            {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required", 'aria-label'=>'Image']) }}
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6">
-        <div class="form-group">
-            <?php
-            $field_name = 'reporter_type';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
-            $select_options = [
-                'Orang Tua' => 'Orang Tua',
-                'Murid' => 'Murid',
-                'Other' => 'Other',
-            ];
-            $required = "required";
-            ?>
-            {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('form-control select2')->attributes(["$required"]) }}
-        </div>
-    </div>
-    <div class="col-lg-6">
-        <div class="form-group">
-            <?php
-            $field_name = 'reporter_email';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
-            $required = "";
-            ?>
-            {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required", 'aria-label'=>'Image']) }}
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-6">
-        <div class="form-group">
-            <?php
-            $field_name = 'kelas';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = '--Silakan pilih--';
-            $select_options = config('kelas');
-            $required = "required";
-            ?>
-            {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('form-control')->attributes(["$required"]) }}
-        </div>
-    </div>
-    <div class="col-lg-6">
-        <div class="form-group">
-            <?php
-            $field_name = 'nomor_hp';
-            $field_lable = __("reporting::$module_name.$field_name");
             $field_placeholder = '';
             $required = "required";
             ?>
@@ -75,13 +16,14 @@
     <div class="col-lg-12">
         <div class="form-group">
             <?php
-            $field_name = 'alamat';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = '';
+            $field_name = 'kelas';
+            $field_lable = __("reporting::$module_name.$field_name")." Anak";
+            $field_placeholder = '--Silakan pilih--';
+            $select_options = config('kelas');
             $required = "required";
             ?>
             {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->text($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required", 'aria-label'=>'Image']) }}
+            {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('form-control')->attributes(["$required"]) }}
         </div>
     </div>
 </div>
@@ -91,7 +33,7 @@
             <?php
             $field_name = 'title';
             $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
+            $field_placeholder = '';
             $required = "required";
             ?>
             {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
@@ -100,46 +42,20 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-6">
-        <div class="form-group">
-            <?php
-            $field_name = 'category';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
-            $select_options = $options['category'];
-            ?>
-            {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('form-control select2')->attributes(["$required"]) }}
-        </div>
-    </div>
-    <div class="col-lg-6">
-        <div class="form-group">
-            <?php
-            $field_name = 'status';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
-            $required = "required";
-            $select_options = $options['status'];
-            ?>
-            {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->select($field_name, $select_options)->placeholder($field_placeholder)->class('form-control select2')->attributes(["$required"]) }}
-        </div>
-    </div>
-</div>
-<div class="row">
     <div class="col-lg-12">
         <div class="form-group">
             <?php
             $field_name = 'content';
-            $field_lable = __("reporting::$module_name.$field_name");
-            $field_placeholder = $field_lable;
+            $field_lable = "Topik Hari ini";
+            $field_placeholder = 'Isi artikel disini';
             $required = "required";
             ?>
             {{ html()->label($field_lable, $field_name) }} {!! fielf_required($required) !!}
-            {{ html()->textarea($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required", 'aria-label'=>'Image', 'rows'=>5]) }}
+            {{ html()->textarea($field_name)->placeholder($field_placeholder)->class('form-control')->attributes(["$required", 'aria-label'=>'Image','rows'=>5]) }}
         </div>
     </div>
 </div>
+
 <!-- Select2 Library -->
 <x-library.select2 />
 <x-library.datetime-picker />
@@ -153,6 +69,7 @@
 
 <!-- Date Time Picker & Moment Js-->
 <script type="text/javascript">
+    
 $(function() {
     var date = moment("{{$$module_name_singular->birth_date ?? ''}}", 'YYYY-MM-DD').toDate();
     $('.datetime').datetimepicker({
